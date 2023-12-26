02e7ff292d84e81c2a38ef2c392c3094ce4c9204
Marc <m.roset.bermudez@gmail.com>
2023-12-26 20:32:53 +0100

    diez primeros numeros con while

---
+++ 01_38_deu_primers/DeuPrimers.java
@@ -0,0 +1,12 @@
+/*
+ * Este programa muestra los 10 primeros numeros naturales con un while
+ */
+public class DeuPrimers {
+    public static void main(String[] args) {
+        int numero = 0;
+        while (numero < 10) {
+            numero ++;
+            System.out.println(numero);
+        }
+    }
+}
