59435595c58067e10e69cc143ef6c8d28ce0fee5
Marc <m.roset.bermudez@gmail.com>
2023-12-26 21:17:22 +0100

    ejercicio de primeros pares

---
+++ 01_40_primers_parells/PrimersParells.java
@@ -0,0 +1,14 @@
+/*
+ * Programa que muestra los primeros numeros pares
+ */
+
+public class PrimersParells {
+    public static void main(String[] args) {
+        int num = 0;
+        
+        while(num < 10) {
+            num += 2;
+            System.out.println(num);
+        }
+    }
+}
