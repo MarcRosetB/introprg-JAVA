3639ea2dccc3a0d034c3c383f092399c68600dc6
Marc <m.roset.bermudez@gmail.com>
2023-12-26 20:40:06 +0100

    diez numeros decrecientes con while

---
+++ 01_39_deu_primers_decreixent/DeuPrimersDecreixent.java
@@ -0,0 +1,12 @@
+/* este programa muestra los 10 primeros numeros de manera decreciente */
+
+
+public class DeuPrimersDecreixent {
+    public static void main(String[] args) {
+        int num = 11;
+        while (num >0) {
+            num --;
+            System.out.println(num);
+        }
+    }
+}
