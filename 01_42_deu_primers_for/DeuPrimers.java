d9fa9e37b01ceffbe5198b4e583a3953f6ff26d7
Marc <m.roset.bermudez@gmail.com>
2023-12-26 21:26:28 +0100

    ejercicio de 10 numeros con for

---
+++ 01_42_deu_primers_for/DeuPrimers.java
@@ -0,0 +1,12 @@
+/*
+ * programa que muestra los 10 primeros numeros con for
+*/
+
+public class DeuPrimers {
+    public static void main(String[] args) {
+        
+        for(int x = 1; x <= 10; x++) {
+            System.out.println(x);
+        }
+    }
+}
