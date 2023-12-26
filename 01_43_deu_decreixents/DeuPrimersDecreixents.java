1d85726271437837a590693ccf066128abd61339
Marc <m.roset.bermudez@gmail.com>
2023-12-26 21:38:10 +0100

    diez numeros decrecientes con for

---
+++ 01_43_deu_decreixents/DeuPrimersDecreixents.java
@@ -0,0 +1,11 @@
+/*
+ * programa que muestra los 10 primeros numeros decrecientes con un for
+*/
+
+public class DeuPrimersDecreixents {
+    public static void main(String[] args) {
+        for(int x= 10; x >=0; x--) {
+            System.out.println(x);
+        }
+    }
+}
