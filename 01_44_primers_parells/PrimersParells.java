60e69de601a0ab37bbddb3a8f835e68096c8983d
Marc <m.roset.bermudez@gmail.com>
2023-12-26 21:54:12 +0100

    primeros pares con bucle for

---
+++ 01_44_primers_parells/PrimersParells.java
@@ -0,0 +1,9 @@
+/*Primeros pares con un bucle for */
+
+public class PrimersParells {
+    public static void main(String[] args) {
+        for(int i = 2; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
