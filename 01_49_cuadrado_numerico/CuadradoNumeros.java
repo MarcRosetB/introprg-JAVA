7c2452504527a93446d1f6e912e4bafcacc44d52
Marc <m.roset.bermudez@gmail.com>
2024-01-07 11:37:59 +0100

    ejercicio recorridos anidados

---
+++ 01_49_cuadrado_numerico/CuadradoNumeros.java
@@ -0,0 +1,21 @@
+/*
+ * Usaremos dos for anidados para formar un cuadrado de numeros
+ */
+
+import java.util.Scanner;
+
+public class CuadradoNumeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Valor final?");
+        int fin = sc.nextInt();
+
+        for(int fila = 1; fila <= fin; fila ++) {
+            for(int col = 1; col <= fin; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+}
